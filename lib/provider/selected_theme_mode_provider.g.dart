eba418fbad630323a0a2d761d14e928a10b35dfd
smsmsy <smsmsy.dev@gmail.com>
2025-06-21 02:25:20 +0900

    ThemeModeの選択値を永続化して保存するProviderの追加

---
+++ lib/provider/selected_theme_mode_provider.g.dart
@@ -0,0 +1,27 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'selected_theme_mode_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$selectedThemeModeHash() => r'59251d24f3155218ead9e3e190c41c3e29ee116f';
+
+/// See also [SelectedThemeMode].
+@ProviderFor(SelectedThemeMode)
+final selectedThemeModeProvider =
+    AutoDisposeAsyncNotifierProvider<SelectedThemeMode, ThemeMode>.internal(
+      SelectedThemeMode.new,
+      name: r'selectedThemeModeProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$selectedThemeModeHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
+
+typedef _$SelectedThemeMode = AutoDisposeAsyncNotifier<ThemeMode>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
