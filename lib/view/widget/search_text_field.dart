8d3aebf82ae9c13c5bfa9daa51aab283df778076
smsmsy <smsmsy.dev@gmail.com>
2025-06-22 02:24:08 +0900

    テキストボックスを追加

---
+++ lib/view/widget/search_text_field.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+
+class SearchTextField extends StatelessWidget {
+  const SearchTextField({
+    super.key,
+    this.controller,
+    this.onChanged,
+    this.onCancelButtonPressed,
+    this.labelText,
+  });
+
+  final TextEditingController? controller;
+  final void Function(String)? onChanged;
+  final void Function()? onCancelButtonPressed;
+  final String? labelText;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: TextField(
+        controller: controller ?? TextEditingController(),
+        decoration: InputDecoration(
+          prefixIcon: const Icon(Icons.search),
+          suffixIcon: IconButton(
+            onPressed: onCancelButtonPressed,
+            icon: const Icon(Icons.cancel),
+          ),
+          labelText: labelText,
+        ),
+        onChanged: onChanged,
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+    properties.add(
+      DiagnosticsProperty<TextEditingController?>('controller', controller),
+    );
+    properties.add(
+      ObjectFlagProperty<void Function(String p1)?>.has('onChanged', onChanged),
+    );
+    properties.add(
+      DiagnosticsProperty<void Function()?>(
+        'onCancelButtonPressed',
+        onCancelButtonPressed,
+      ),
+    );
+    properties.add(StringProperty('labelText', labelText));
+  }
+}
