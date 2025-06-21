c520bf44999b1ca93043fb69421787fad9bda32a
smsmsy <smsmsy.dev@gmail.com>
2025-06-22 06:11:29 +0900

    リストビューのスクロール時にAppBarの背景色が変わってしまう問題に対応、さらにAppBarのデザインをFIXする

--- lib/provider/custom_theme_data_provider.dart
+++ lib/provider/custom_theme_data_provider.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 final ProviderFamily<ThemeData, Brightness> customThemeDataProvider =
@@ -11,6 +12,8 @@ final ProviderFamily<ThemeData, Brightness> customThemeDataProvider =
         return ThemeData(
           useMaterial3: true,
           colorScheme: colorScheme,
+
+          // テキストフィールド類のテーマ設定を行う
           inputDecorationTheme: InputDecorationTheme(
             border: OutlineInputBorder(
               borderRadius: BorderRadius.circular(8),
@@ -21,6 +24,25 @@ final ProviderFamily<ThemeData, Brightness> customThemeDataProvider =
               }
               return colorScheme.secondary;
             }),
+          ),
+
+          // AppBarのテーマ設定を行う
+          appBarTheme: AppBarTheme(
+            backgroundColor: colorScheme.surface,
+            foregroundColor: colorScheme.onSurface,
+            titleTextStyle: TextStyle(
+              color: colorScheme.onSurface,
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
+            ),
+            // ステータスバーの色とアイコンの明るさを設定
+            systemOverlayStyle: SystemUiOverlayStyle(
+              statusBarColor: colorScheme.surface,
+              statusBarIconBrightness: brightness,
+              statusBarBrightness: brightness,
+            ),
+            // ボディをスクロールしたとしても背景の色が変わらないようにする
+            scrolledUnderElevation: 0,
           ),
         );
       },
