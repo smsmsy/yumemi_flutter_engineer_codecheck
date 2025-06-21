5c1b5866de6fa6eb71b40b74254b82d467daaf10
smsmsy <smsmsy.dev@gmail.com>
2025-06-22 07:34:54 +0900

    クエリーの更新実装を追加、onChangedを一旦コメントアウト（デバウンス対応した後で解除する）

--- lib/view/page/search_page.dart
+++ lib/view/page/search_page.dart
@@ -1,19 +1,21 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:yumemi_flutter_engineer_codecheck/domain/model/git_hub_search_api/git_hub_search_query.dart';
 import 'package:yumemi_flutter_engineer_codecheck/l10n/app_localizations.dart';
 import 'package:yumemi_flutter_engineer_codecheck/static/wording_data.dart';
 import 'package:yumemi_flutter_engineer_codecheck/view/widget/custom_drawer.dart';
 import 'package:yumemi_flutter_engineer_codecheck/view/widget/search_result_list_view.dart';
 import 'package:yumemi_flutter_engineer_codecheck/view/widget/search_text_field.dart';
 
-class SearchPage extends StatefulWidget {
+class SearchPage extends ConsumerStatefulWidget {
   const SearchPage({super.key});
 
   @override
-  State<SearchPage> createState() => _SearchPageState();
+  ConsumerState<SearchPage> createState() => _SearchPageState();
 }
 
-class _SearchPageState extends State<SearchPage> {
+class _SearchPageState extends ConsumerState<SearchPage> {
   var _text = '';
 
   final controller = TextEditingController();
@@ -37,24 +39,32 @@ class _SearchPageState extends State<SearchPage> {
             children: [
               SearchTextField(
                 controller: controller,
-                onChanged: (value) {
-                  // TODO: リポジトリ検索実行時の実装
-                  setState(() {
-                    _text = value;
-                  });
-                },
+                // TODO : onChanged を使用すると、入力中にAPIを叩いてしまうため、現状は onSubmitted のみを使用する。
+                // TODO : 例えば、Debounceを使用して、一定時間入力がない場合にAPIを叩くようにするなどして改善後にコメント解除する。
+                // onChanged: (value) {
+                //   setState(() {
+                //     _text = value;
+                //   });
+                //   ref
+                //       .read(gitHubSearchQueryNotifierProvider.notifier)
+                //       .setQuery(q: _text);
+                // },
                 onSubmitted: (value) {
-                  // TODO: エンターキーや検索ボタンでの検索実行時の実装
                   setState(() {
                     _text = value;
                   });
+                  ref
+                      .read(gitHubSearchQueryNotifierProvider.notifier)
+                      .setQuery(q: _text);
                 },
                 onCancelButtonPressed: () {
-                  // TODO: リポジトリ検索キャンセル時の実装
                   setState(() {
                     _text = '';
                     controller.clear();
                   });
+                  ref
+                      .read(gitHubSearchQueryNotifierProvider.notifier)
+                      .setQuery(q: '');
                 },
                 labelText:
                     AppLocalizations.of(context)?.searchRepositories ??
