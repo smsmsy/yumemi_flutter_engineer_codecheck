0ebafc7f3c0e457b79fb7d8dcc19ec1f0f8af3fd
smsmsy <smsmsy.dev@gmail.com>
2025-06-22 02:31:07 +0900

    テキストフィールドを検索画面に追加

--- lib/view/page/search_page.dart
+++ lib/view/page/search_page.dart
@@ -1,10 +1,21 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:yumemi_flutter_engineer_codecheck/l10n/app_localizations.dart';
 import 'package:yumemi_flutter_engineer_codecheck/static/wording_data.dart';
 import 'package:yumemi_flutter_engineer_codecheck/view/widget/custom_drawer.dart';
+import 'package:yumemi_flutter_engineer_codecheck/view/widget/search_text_field.dart';
 
-class SearchPage extends StatelessWidget {
+class SearchPage extends StatefulWidget {
   const SearchPage({super.key});
+
+  @override
+  State<SearchPage> createState() => _SearchPageState();
+}
+
+class _SearchPageState extends State<SearchPage> {
+  var _text = '';
+
+  final controller = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -16,6 +27,42 @@ class SearchPage extends StatelessWidget {
         ),
       ),
       drawer: const CustomDrawer(),
+      body: Center(
+        child: Column(
+          children: [
+            SearchTextField(
+              controller: controller,
+              onChanged: (value) {
+                // TODO: リポジトリ検索実行時の実装
+                setState(() {
+                  _text = value;
+                });
+              },
+              onCancelButtonPressed: () {
+                // TODO: リポジトリ検索キャンセル時の実装
+                setState(() {
+                  _text = '';
+                  controller.clear();
+                });
+                FocusScope.of(context).unfocus();
+              },
+              labelText:
+                  AppLocalizations.of(context)?.searchRepositories ??
+                  WordingData.searchRepositories,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  void debugFillProperties(DiagnosticPropertiesBuilder properties) {
+    super.debugFillProperties(properties);
+
+    properties.add(StringProperty('text', _text));
+    properties.add(
+      DiagnosticsProperty<TextEditingController>('controller', controller),
     );
   }
 }
