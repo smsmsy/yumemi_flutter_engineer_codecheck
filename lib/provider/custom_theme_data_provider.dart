4e99743ceaecfbb1cfcde2f552bed1d9d708c835
smsmsy <smsmsy.dev@gmail.com>
2025-06-22 02:30:43 +0900

    テキストボックスのテーマを設定

--- lib/provider/custom_theme_data_provider.dart
+++ lib/provider/custom_theme_data_provider.dart
@@ -4,11 +4,23 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 final ProviderFamily<ThemeData, Brightness> customThemeDataProvider =
     Provider.family<ThemeData, Brightness>(
       (ref, brightness) {
+        final colorScheme = ColorScheme.fromSeed(
+          brightness: brightness,
+          seedColor: const Color(0xFF1158c7),
+        );
         return ThemeData(
           useMaterial3: true,
-          colorScheme: ColorScheme.fromSeed(
-            brightness: brightness,
-            seedColor: const Color(0xFF1158c7),
+          colorScheme: colorScheme,
+          inputDecorationTheme: InputDecorationTheme(
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(8),
+            ),
+            prefixIconColor: WidgetStateColor.resolveWith((states) {
+              if (states.contains(WidgetState.focused)) {
+                return colorScheme.primary;
+              }
+              return colorScheme.secondary;
+            }),
           ),
         );
       },
