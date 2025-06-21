cb40ac084d1a7b67388d4e7296f3f9335caf0fe1
smsmsy <smsmsy.dev@gmail.com>
2025-06-21 16:07:32 +0900

    ベースとなる文言リソースファイルを追加

---
+++ lib/l10n/app_localizations_en.dart
@@ -0,0 +1,22 @@
+// ignore: unused_import
+import 'package:intl/intl.dart' as intl;
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get appTitle => 'Flutter Demo';
+
+  @override
+  String get homePageTitle => 'Flutter Demo Home Page';
+
+  @override
+  String get counterText => 'You have pushed the button this many times:';
+
+  @override
+  String get incrementTooltip => 'Increment';
+}
