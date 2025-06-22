cc30e6ed3dca7eb508166ba6095c35812f99c18f
smsmsy <smsmsy.dev@gmail.com>
2025-06-22 11:07:09 +0900

    KeepAiive: trueに指定(両方)とdioを引き取るように変更(apiRepositoryだけ)

--- lib/domain/model/git_hub_search_api/git_hub_search_query.g.dart
+++ lib/domain/model/git_hub_search_api/git_hub_search_query.g.dart
@@ -7,24 +7,22 @@ part of 'git_hub_search_query.dart';
 // **************************************************************************
 
 String _$gitHubSearchQueryNotifierHash() =>
-    r'6a00311445c6b3e1d8c773cf0b83db0014577a14';
+    r'745bb2a26f8d0a5ab6a491b1922a482ecae2e5c2';
 
 /// See also [GitHubSearchQueryNotifier].
 @ProviderFor(GitHubSearchQueryNotifier)
-final gitHubSearchQueryNotifierProvider = AutoDisposeNotifierProvider<
-  GitHubSearchQueryNotifier,
-  GitHubSearchQuery
->.internal(
-  GitHubSearchQueryNotifier.new,
-  name: r'gitHubSearchQueryNotifierProvider',
-  debugGetCreateSourceHash:
-      const bool.fromEnvironment('dart.vm.product')
-          ? null
-          : _$gitHubSearchQueryNotifierHash,
-  dependencies: null,
-  allTransitiveDependencies: null,
-);
+final gitHubSearchQueryNotifierProvider =
+    NotifierProvider<GitHubSearchQueryNotifier, GitHubSearchQuery>.internal(
+      GitHubSearchQueryNotifier.new,
+      name: r'gitHubSearchQueryNotifierProvider',
+      debugGetCreateSourceHash:
+          const bool.fromEnvironment('dart.vm.product')
+              ? null
+              : _$gitHubSearchQueryNotifierHash,
+      dependencies: null,
+      allTransitiveDependencies: null,
+    );
 
-typedef _$GitHubSearchQueryNotifier = AutoDisposeNotifier<GitHubSearchQuery>;
+typedef _$GitHubSearchQueryNotifier = Notifier<GitHubSearchQuery>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
