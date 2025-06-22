6b3ba584fff55dbc13f158e1d0d055ec8d32f891
smsmsy <smsmsy.dev@gmail.com>
2025-06-22 12:09:18 +0900

    dioとTImerのDIとコメントの拡充

--- lib/extension/debounce_and_cancel_extension.dart
+++ lib/extension/debounce_and_cancel_extension.dart
@@ -3,25 +3,33 @@ import 'dart:async';
 import 'package:dio/dio.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
+/// テストや共通設定のためにDioやTimerの注入を可能にした拡張
 extension DebounceAndCancelExtension on Ref {
   /// デバウンス・キャンセル処理を実装した [Dio] を返します。
   ///
-  /// - デバウンス処理とは、ユーザーが入力をやめてから一定時間後にリクエストを送信することです。
-  ///   例えば、ユーザーがテキストボックスに入力している最中にリクエストを送信しないようにするために使用されます。
-  ///   この処理により、ユーザーが入力を完了してから500ms経ったあとにリクエストが送信されます。
-  ///   デフォルトの待機時間は500msです。一般的には 500ms 程度で十分とされていますが、必要に応じて変更可能です。
+  /// [dio] でDioインスタンスを注入できます。未指定時は新規生成。
+  /// [timerFactory] でTimerの生成方法を注入できます（テスト用）。
   ///
-  /// - キャンセル処理とは、ユーザーがリクエスト中に値の監視をやめた場合にリクエストをキャンセルすることです。
-  ///   例えば、ユーザーがテキストボックスに入力中にダイアログごと閉じた時などに発動します。
-  ///   この処理により、ユーザーがリクエスト中に値の監視をやめた場合、リクエストはキャンセルされます。
-  /// 
+  /// - デバウンス処理とは、ユーザーが入力をやめてから一定時間後にリクエストを送信することです。<BR>
+  ///   例えば、ユーザーがテキストボックスに入力している最中にリクエストを送信しないようにするために使用されます。<BR>
+  ///   この処理により、ユーザーが入力を完了してから500ms経ったあとにリクエストが送信されます。<BR>
+  ///   デフォルトの待機時間は500msです。一般的には 500ms 程度で十分とされていますが、必要に応じて変更可能です。<BR>
+  ///
+  /// - キャンセル処理とは、ユーザーがリクエスト中に値の監視をやめた場合にリクエストをキャンセルすることです。<BR>
+  ///   例えば、ユーザーがテキストボックスに入力中にダイアログごと閉じた時などに発動します。<BR>
+  ///   この処理により、ユーザーがリクエスト中に値の監視をやめた場合、リクエストはキャンセルされます。<BR>
+  ///
   /// 参考 : https://riverpod.dev/docs/case_studies/cancel#going-further-doing-both-at-once
-  Future<Dio> getDebouncedHttpClient([Duration? duration]) async {
+  Future<Dio> getDebouncedHttpClient({
+    Duration? duration,
+    Dio? dio,
+    Timer Function(Duration duration, void Function() callback)? timerFactory,
+  }) async {
     var didDispose = false;
     onDispose(() => didDispose = true);
 
     final completer = Completer<void>();
-    final timer = Timer(
+    final timer = (timerFactory ?? Timer.new)(
       duration ?? const Duration(milliseconds: 500),
       completer.complete,
     );
@@ -38,9 +46,9 @@ extension DebounceAndCancelExtension on Ref {
       throw Exception('Cancelled');
     }
 
-    final dio = Dio();
-    onDispose(dio.close);
+    final client = dio ?? Dio();
+    onDispose(client.close);
 
-    return dio;
+    return client;
   }
 }
