816f21b7e435abca7eff64175be5ed062a797e02
smsmsy <smsmsy.dev@gmail.com>
2025-06-26 22:27:17 +0900

    不要なコードを削除

--- test/features/repository_search/presentation/common/widget/owner_icon_test.dart
+++ test/features/repository_search/presentation/common/widget/owner_icon_test.dart
@@ -1,46 +1,8 @@
-import 'dart:ui' as ui;
-
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:yumemi_flutter_engineer_codecheck/features/repository_search/domain/entity/owner.dart';
 import 'package:yumemi_flutter_engineer_codecheck/features/repository_search/domain/entity/repository.dart';
 import 'package:yumemi_flutter_engineer_codecheck/features/repository_search/presentation/common/widget/owner_icon.dart';
-
-class FakeLoadingImage extends Image {
-  const FakeLoadingImage({super.key})
-    : super(image: const FakeLoadingImageProvider());
-}
-
-class FakeLoadingImageProvider extends ImageProvider<FakeLoadingImageProvider> {
-  const FakeLoadingImageProvider();
-
-  @override
-  Future<FakeLoadingImageProvider> obtainKey(ImageConfiguration configuration) {
-    return SynchronousFuture<FakeLoadingImageProvider>(this);
-  }
-
-  @override
-  ImageStreamCompleter loadImage(
-    FakeLoadingImageProvider key,
-    ImageDecoderCallback decode,
-  ) {
-    // ローディング状態を維持するため、完了しないImageStreamCompleterを返す
-    return OneFrameImageStreamCompleter(
-      Future.delayed(
-        const Duration(seconds: 1),
-        () => ImageInfo(image: _createTestImage()),
-      ),
-    );
-  }
-
-  ui.Image _createTestImage() {
-    final recorder = ui.PictureRecorder();
-    final canvas = Canvas(recorder);
-    canvas.drawRect(const Rect.fromLTWH(0, 0, 1, 1), Paint());
-    return recorder.endRecording().toImageSync(1, 1);
-  }
-}
 
 void main() {
   setUp(TestWidgetsFlutterBinding.ensureInitialized);
