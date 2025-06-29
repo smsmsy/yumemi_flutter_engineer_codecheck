376e13009220530ab8f0707afab7d49e61f8d861
smsmsy <smsmsy.dev@gmail.com>
2025-06-29 15:13:05 +0900

    不要な宣言を削除（このコミットは動かないがコミットログ整理のために一旦コミットします。）

--- lib/features/repository_search/presentation/provider/repository_providers.g.dart
+++ lib/features/repository_search/presentation/provider/repository_providers.g.dart
@@ -198,27 +198,5 @@ final repositoriesSearchResultProvider =
 // ignore: unused_element
 typedef RepositoriesSearchResultRef =
     AutoDisposeFutureProviderRef<List<Repository>>;
-String _$gitHubSearchQueryNotifierHash() =>
-    r'745bb2a26f8d0a5ab6a491b1922a482ecae2e5c2';
-
-/// GitHub検索クエリの状態管理プロバイダー
-///
-/// ユーザーの検索入力を管理し、UIレイヤーから利用されます。
-///
-/// Copied from [GitHubSearchQueryNotifier].
-@ProviderFor(GitHubSearchQueryNotifier)
-final gitHubSearchQueryNotifierProvider =
-    NotifierProvider<GitHubSearchQueryNotifier, GitHubSearchQuery>.internal(
-      GitHubSearchQueryNotifier.new,
-      name: r'gitHubSearchQueryNotifierProvider',
-      debugGetCreateSourceHash:
-          const bool.fromEnvironment('dart.vm.product')
-              ? null
-              : _$gitHubSearchQueryNotifierHash,
-      dependencies: null,
-      allTransitiveDependencies: null,
-    );
-
-typedef _$GitHubSearchQueryNotifier = Notifier<GitHubSearchQuery>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
