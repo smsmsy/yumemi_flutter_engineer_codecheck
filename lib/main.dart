dd435cb7259b512da2b29bc62f31342a255e0b20
smsmsy <smsmsy.dev@gmail.com>
2025-06-29 20:33:40 +0900

    Firebaseの初期化をmain関数に追加し、非同期処理に対応しました

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,19 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yumemi_flutter_engineer_codecheck/features/repository_search/presentation/provider/custom_theme_data_provider.dart';
 import 'package:yumemi_flutter_engineer_codecheck/features/repository_search/presentation/provider/go_router_provider.dart';
 import 'package:yumemi_flutter_engineer_codecheck/features/repository_search/presentation/provider/selected_theme_mode_provider.dart';
+import 'package:yumemi_flutter_engineer_codecheck/firebase_options.dart';
 import 'package:yumemi_flutter_engineer_codecheck/l10n/app_localizations.dart';
 import 'package:yumemi_flutter_engineer_codecheck/static/wording_data.dart';
 
 /// アプリケーションのエントリーポイントです。
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
